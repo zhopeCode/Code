3da018daf0da7e517931649a507be7e0b55aa69c
zhopeCode <117990406+zhopeCode@users.noreply.github.com>
2023-03-08 22:25:05 +0800

    Update test_3_3.c

--- test_3_3.c
+++ test_3_3.c
@@ -4,13 +4,13 @@
 //等以后技术上去了再改改
 int main()
 {
-	long long password1;
-	long long password2;
+	char password1[20];
+	char password2[20];
 	printf("请输入密码:>");
-	scanf("%lld", &password1);
+	scanf("%s", &password1);
 	printf("请再次输入密码:>");
-	scanf("%lld", &password2);
-	if (password1 == password2)
+	scanf("%s", &password2);
+	if (strcmp(password1, password2) == 1)
 		printf("确认密码成功");
 	else
 		printf("两次密码不一致");
