9afec286c53a15600b87f21c39feee1544c1b3a0
zhopeCode <117990406+zhopeCode@users.noreply.github.com>
2023-03-08 22:53:19 +0800

    Update test_3_3.c

--- test_3_3.c
+++ test_3_3.c
@@ -1,7 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
-//现在还只能检测纯数字
-//等以后技术上去了再改改
+#include <string.h>
+//现在可以检测数字加字符
 int main()
 {
 	char password1[20];
@@ -10,7 +10,7 @@ int main()
 	scanf("%s", &password1);
 	printf("请再次输入密码:>");
 	scanf("%s", &password2);
-	if (strcmp(password1, password2) == 1)
+	if (strcmp(password1, password2) == 0)
 		printf("确认密码成功");
 	else
 		printf("两次密码不一致");
