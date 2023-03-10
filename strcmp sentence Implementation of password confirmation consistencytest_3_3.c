863c51f60d5424cff9f0160fc08fe76f6d14d807
zhopeCode <117990406+zhopeCode@users.noreply.github.com>
2023-03-10 19:55:18 +0800

    Update strcmp sentence Implementation of password confirmation consistencytest_3_3.c

--- strcmp sentence Implementation of password confirmation consistencytest_3_3.c
+++ strcmp sentence Implementation of password confirmation consistencytest_3_3.c
@@ -4,13 +4,13 @@
 //现在可以检测数字加字符
 int main()
 {
-	char password1[20];
-	char password2[20];
+	char firstPassword[20];
+	char secondPassword[20];
 	printf("请输入密码:>");
-	scanf("%s", &password1);
+	scanf("%s", &firstPassword);
 	printf("请再次输入密码:>");
-	scanf("%s", &password2);
-	if (strcmp(password1, password2) == 0)
+	scanf("%s", &secondPassword);
+	if (strcmp(firstPassword, secondPassword) == 0)
 	{
 		printf("确认密码成功");
 	}
