3c30b76bed55188e82f5b8dd5d794edb73d32e60
zhopeCode <117990406+zhopeCode@users.noreply.github.com>
2023-03-10 19:25:50 +0800

    Update Binary Search test_3_7.c

--- Binary Search test_3_7.c
+++ Binary Search test_3_7.c
@@ -3,29 +3,29 @@
 //二分查找法
 int main()
 {
-	int arr[] = { 0,1,2,3,4,5,6,7,8,9,10 };//整形有序数组
-	int left = 0;//范围左
-	int right = (sizeof arr / sizeof arr[0]) - 1;//范围右
-	int mid = 0;//查找下标
-	int num = 10;//被查找数
-	while (left <= right)
+	int userArr[] = { 0,1,2,3,4,5,6,7,8,9,10 };//整形有序数组
+	int userLeft = 0;//范围左
+	int userright = (sizeof userArr / sizeof userArr[0]) - 1;//范围右
+	int userMid = 0;//查找下标
+	int userNum = 10;//被查找数
+	while (userLeft <= userright)
 	{
-		mid = (left + right) / 2;//进行数组的二分
-		if (arr[mid] > num)//将右范围缩小
+		userMid = (userLeft + userright) / 2;//进行数组的二分
+		if (userArr[userMid] > userNum)//将右范围缩小
 		{
-			right = mid - 1;
+			userright = userMid - 1;
 		}
-		else if (arr[mid] < num)//将左范围缩小
+		else if (userArr[userMid] < userNum)//将左范围缩小
 		{
-			left = mid + 1;
+			userLeft = userMid + 1;
 		}
 		else//找到
 		{
-			printf("数组下标为:%d\n", mid);
+			printf("数组下标为:%d\n", userMid);
 			break;
 		}
 	}
-	if (left > right)//没找到
+	if (userLeft > userright)//没找到
 	{
 		printf("找不到");
 	}
