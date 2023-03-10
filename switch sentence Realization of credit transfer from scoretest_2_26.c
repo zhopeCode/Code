9a38541af3a75f0beed7d31c3d6d5cd334bf9ac9
zhopeCode <117990406+zhopeCode@users.noreply.github.com>
2023-03-10 19:58:35 +0800

    Update switch sentence Realization of credit transfer from scoretest_2_26.c

--- switch sentence Realization of credit transfer from scoretest_2_26.c
+++ switch sentence Realization of credit transfer from scoretest_2_26.c
@@ -2,27 +2,28 @@
 #include <stdio.h>
 int main()
 {
-	int score, s; char grade;					//定义变量
+	int userScore, sum;
+	char userGrade;					//定义变量
 	printf(" 请输入分数：");					//提升输入分数
-	scanf("%d", &score);						//接收分数
-	if (score >= 0 && score <= 100)	//如果是一个正确的百分制的成绩
+	scanf("%d", &userScore);						//接收分数
+	if (userScore >= 0 && userScore <= 100)	//如果是一个正确的百分制的成绩
 	{
-		s = score / 10;							//成绩整除10
-		switch (s)
+		sum = userScore / 10;							//成绩整除10
+		switch (sum)
 		{
 		case 0:
 		case 1:
 		case 2:
 		case 3:
 		case 4:
-		case 5:grade = '2'; break;	//60分以下的转换为2分
+		case 5:userGrade = '2'; break;	//60分以下的转换为2分
 		case 6:
-		case 7:grade = '3'; break;	//80分以下的转换为3分
-		case 8:grade = '4'; break;	//90分以下的转换为4分
+		case 7:userGrade = '3'; break;	//80分以下的转换为3分
+		case 8:userGrade = '4'; break;	//90分以下的转换为4分
 		case 9:								//100分以下和100分转化为5分
-		case 10:grade = '5'; break;
+		case 10:userGrade = '5'; break;
 		}
-		printf("分数等级是%c\n", grade);	//输出五分制成绩
+		printf("分数等级是%c\n", userGrade);	//输出五分制成绩
 	}
 	else
 	{
